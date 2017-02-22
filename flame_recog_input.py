55f62bff1fdc521467129c62e5604b1e4dba6e91
chirateep <tabechi155@gmail.com>
2017-02-22 20:40:49 +0700

    Add file

---
+++ flame_recog_input.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__	import print_function
+
+import os
+from six.moves import xrange
+import tensorflow as tf
+
+IMAGE_SIZE = 
